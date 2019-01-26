600c33e4ea1a24827feaf0c6408935071bc3fc9d
Isaac Doré <swessmusic@gmail.com>
2019-01-26 08:47:38 -0500

    Add Input Context Initializer

---
+++ Assets/_Scripts/Core/Inputs/InputContextInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Core.Inputs;
+using UnityEngine;
+
+
+/// <summary>
+/// Add this component to a GameObject present at load of the scene
+/// to make the context apply on Start.
+/// EX: The Camera
+/// </summary>
+public class InputContextInitializer : MonoBehaviour {
+
+    public ContextualInputsState inputContext;
+
+    void Start() {
+        if(inputContext != null)
+            Core.GameController.Instance.actionsMapsHelper.ApplyContext(inputContext);
+    }
+
+}
