f87e75d8aa0fbdfc5bada30eaf7e699964b6e572
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2019-01-26 15:22:38 -0500

    Fixed ratio

--- Assets/_Scripts/CircleManager.cs
+++ Assets/_Scripts/CircleManager.cs
@@ -5,6 +5,7 @@ using Rewired;
 public class CircleManager : MonoBehaviour
 {
     public  int          nTriangles = 20;
+    public int correction = 0;
     public  float        thetaScale = 0.01f;
     public  float        radius     = 3f;
     public  GameObject   prefab;
@@ -17,21 +18,53 @@ public class CircleManager : MonoBehaviour
         _player1Input = Core.GameController.Instance.actionsMapsHelper.Player1Inputs;
         _piePart      = new GameObject[nTriangles];
         SpawnChildren();
+        flipIt();
     }
 
 
-    private void Update() { return getAngle(); }
+    private void flipIt() { transform.rotation = Quaternion.Euler(0, 180, 0); }
+
+
+    private void Update() {
+        UpdateLookAt(); 
+    }
+
+
+    private void UpdateLookAt() {
+        int realIndex = getIndex();
+        foreach ( GameObject go in _piePart ) {
+            go.SetActive(true);
+        }
+
+        if ( realIndex == -1 ) {
+            return;
+        }
+
+        _piePart[realIndex].SetActive(false);
+    }
+
+
+    private int getIndex() {
+        float angle = getAngle();
+        if ( angle > 360 ) return -1;
+        int                 geniusCompute = 360 / nTriangles;
+        int                 index         = (int) (getAngle() / geniusCompute);
+        CustomRealmOfNumber realIndex     = new CustomRealmOfNumber(index);
+        realIndex.Add(5);
+        return realIndex.Value;
+    }
 
 
     private float getAngle() {
-        if ( _player1Input == null ) return;
+        if ( _player1Input == null ) return 1000f;
 
         float   horizontal = _player1Input.GetAxisRaw(RewiredConsts.Action.Horizontal);
         float   vertical   = _player1Input.GetAxisRaw(RewiredConsts.Action.Vertical);
         Vector2 pVector2   = new Vector2(horizontal, vertical);
 
-        float angle = 0;
-
+        float angle ;
+        if ( pVector2.Equals(Vector2.zero) ) return 1000f;
+        Debug.Log(pVector2);
         if ( pVector2.x < 0 ) {
             angle = 360 - (Mathf.Atan2(pVector2.x, pVector2.y) * Mathf.Rad2Deg * -1);
         } else {
---
+++ Assets/_Scripts/CustomRealmOfNumber.cs
@@ -0,0 +1,36 @@
+﻿public class CustomRealmOfNumber {
+
+    private const int MIN = 0;
+    private const int MAX = 20;
+    private int _value;
+
+    public CustomRealmOfNumber(int value) { _value = value; }
+
+    public int Value {
+        get { return _value; }
+        set { this._value = value; }
+    }
+
+
+    public void Add(int add) {
+        _value = _value + add;
+        if ( _value >= MAX ) {
+            _value = _value - MAX;
+        } else if ( _value < MIN ) {
+            _value = _value + MAX;
+        }
+    }
+
+
+//    public int Reflection() {
+//        _value = _value + (MAX / 2);
+//        if ( _value > MAX ) {
+//            _value = _value - MAX;
+//        } else if ( _value < MIN ) {
+//            _value = _value + MAX;
+//        }
+//
+//        return _value;
+//    }
+    
+}
