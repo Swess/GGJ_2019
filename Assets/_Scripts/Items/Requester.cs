704d501a48e864398846800fbe649512d849f9bf
Isaac Doré <swessmusic@gmail.com>
2019-01-26 20:08:09 -0500

    Bubble Following V1

--- Assets/_Scripts/Items/Requester.cs
+++ Assets/_Scripts/Items/Requester.cs
@@ -9,9 +9,10 @@ public class Requester : MonoBehaviour {
 
     private const string REQUEST_NONE = "NONE";
 
-    public int      askingEachNSeconds = 20;
-    public int      fulfillingTime     = 40;
-    public string[] requestingTag;
+    public int           askingEachNSeconds = 20;
+    public int           fulfillingTime     = 40;
+    public string[]      requestingTag;
+    public RectTransform bubbleFrame;
 
     private float       _timer = 0;
     private IEnumerator _coroutine;
@@ -31,8 +32,32 @@ public class Requester : MonoBehaviour {
     private void Update() {
         if ( _timer > askingEachNSeconds && _requested.Equals(REQUEST_NONE) ) RequestItem();
 
-        if ( _timer > askingEachNSeconds + fulfillingTime )
-            GameController.Instance.SceneController.FadeAndLoadScene("MainMenu");
+        if ( _timer > askingEachNSeconds + fulfillingTime ) GameController.Instance.SceneController.FadeAndLoadScene("MainMenu");
+
+        MoveAlertBubble();
+    }
+
+
+    private void MoveAlertBubble() {
+        if ( Camera.main == null ) return;
+
+        Vector3 screenPos       = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(GameController.Instance.Player1.transform.position);
+
+        Vector3 direction = playerScreenPos - screenPos;
+        float   angle     = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        Vector2 finalScreenPos = new Vector2(screenPos.x, screenPos.y);
+
+        // Clamp to screen
+        if ( finalScreenPos.x >= Screen.width ) finalScreenPos.x = Screen.width;
+        if ( finalScreenPos.x <= 0 ) finalScreenPos.x            = 0f;
+
+        if ( finalScreenPos.y >= Screen.height ) finalScreenPos.y = Screen.height;
+        if ( finalScreenPos.y <= 0 ) finalScreenPos.y             = 0f;
+
+        bubbleFrame.position = new Vector3(finalScreenPos.x, finalScreenPos.y, 0);
+        bubbleFrame.rotation = Quaternion.Euler(0, 0, angle + 90);
     }
 
 
@@ -49,10 +74,9 @@ public class Requester : MonoBehaviour {
         // Ask the opposite of player's holding item
         if ( _player.GetCurrentObjectTag().Equals("Food_Item") )
             _requested = "Wood_Item";
-        else if( _player.GetCurrentObjectTag().Equals("Wood_Item") ) {
+        else if ( _player.GetCurrentObjectTag().Equals("Wood_Item") ) {
             _requested = "Food_Item";
         } else {
-
             // Random otherwise
             _requested = requestingTag[Random.Range(0, requestingTag.Length - 1)];
         }
