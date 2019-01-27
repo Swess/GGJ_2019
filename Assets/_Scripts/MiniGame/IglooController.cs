fe6f1efc7f3d9f8fe479e2ea422d38c09cfc48d7
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2019-01-27 01:50:34 -0500

    Igloos Array and increase

--- Assets/_Scripts/MiniGame/IglooController.cs
+++ Assets/_Scripts/MiniGame/IglooController.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class IglooController : MonoBehaviour {
 
+    public GameObject[] igloos;
+    private int _currentStageIgloo;
+    private Transform _visuals;
     // Start is called before the first frame update
-    void Start() { }
-
-
+    void Start() {
+        _visuals = transform.Find("Visuals");
+        Instantiate(igloos[_currentStageIgloo], _visuals);
+    }
     // Update is called once per frame
-    void Update() { }
+    void Update() {
+    }
 
+
+    private void increaseCurrentStageIgloo() {
+        if ( _currentStageIgloo == igloos.Length ) {
+            Debug.LogError("YOU ARE TRYING TO GO OVER 9000!!!");
+            return    ;
+        }
+        Destroy(_visuals.GetChild(0).gameObject);
+        Instantiate(igloos[++_currentStageIgloo], _visuals);
+    }
 }
