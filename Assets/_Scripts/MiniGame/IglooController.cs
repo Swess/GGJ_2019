8576e6011ca1512808e2c6a1b405789969e539e0
Isaac Doré <swessmusic@gmail.com>
2019-01-27 01:40:42 -0500

    Basic logic frame

--- Assets/_Scripts/MiniGame/IglooController.cs
+++ Assets/_Scripts/MiniGame/IglooController.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Entities.Player;
 using UnityEngine;
 
 public class IglooController : MonoBehaviour {
 
+    public CircleManager circleManager;
+    private bool _inMiniGame = false;
+    private PlayerController _playerController;
+
     // Start is called before the first frame update
-    void Start() { }
+    void Start() {
+        _playerController = Core.GameController.Instance.Player1.GetComponent<PlayerController>();
+        circleManager.gameObject.SetActive(false);
+    }
 
 
     // Update is called once per frame
     void Update() { }
+
+
+    private void OnTriggerEnter(Collider other) {
+        if ( !other.CompareTag("Player") ) return;
+
+        _inMiniGame = true;
+        circleManager.gameObject.SetActive(true);
+        _playerController.SetControlsActive(false);    // Remove Player controls
+    }
+
+
+    private void QuitMiniGame() {
+        _inMiniGame = false;
+        circleManager.gameObject.SetActive(false);
+        _playerController.SetControlsActive(true);     // Reset Game Controls
+    }
 
 }
