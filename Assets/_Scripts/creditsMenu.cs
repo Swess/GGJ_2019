a53c622dd3b6f29d05ba25080c09b356370b287f
Kerry Gougeon Ducharme <kerrygougeon@gmail.com>
2019-01-27 12:17:27 -0500

    Scene manager loop working perfectllyyyy

---
+++ Assets/_Scripts/creditsMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditsMenu : MonoBehaviour
+{
+	public Animator anim;
+	public Rewired.Player controller;
+
+	void Start(){
+		controller = Core.GameController.Instance.actionsMapsHelper.Player1Inputs;
+	}
+    // Update is called once per frame
+    void Update()
+    {
+		if (controller.GetButtonDown(RewiredConsts.Action.UICancel)){
+			CreditsOff();
+		}
+    }
+
+	public void Credits (){
+		anim.SetBool ("CreditsIN", true);
+	}
+	public void CreditsOff (){
+		anim.SetBool ("CreditsIN", false);
+	}
+	public void StartGame(){
+		Core.GameController.Instance.SceneController.FadeAndLoadScene("JF_MainLevel");
+	}
+	public void Quit(){
+		Application.Quit();
+	}
+}
